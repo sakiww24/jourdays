49546e37158c324528e090fc2690565a35dfe400
Xthoa <2733793945@qq.com>
2026-09-20 10:11:30 +0800

    调教router行为

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import ScheduleView from '../components/ScheduleView.vue';
 import TodoView from '../components/TodoView.vue';
 import ProfileView from '../components/ProfileView.vue';
@@ -13,7 +13,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
